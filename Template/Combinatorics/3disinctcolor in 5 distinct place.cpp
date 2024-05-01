8504af877d24befb4040da9e66d11d91b282429a
Monna4335 <79435214+Monna4335@users.noreply.github.com>
2024-05-01 19:31:00 +0600

    Update 3disinctcolor in 5 distinct place.cpp

--- Template/Combinatorics/3disinctcolor in 5 distinct place.cpp
+++ Template/Combinatorics/3disinctcolor in 5 distinct place.cpp
@@ -17,3 +17,20 @@ long long count(long long place,long long color){
 	}
 	return plc[place][color]=cnt;
 }
+//another way
+
+long long plc[455][53];
+long long count(long long place,long long color){
+	if(place<color)return 0;
+	if(place==0)return 0;
+	if(color==0)return 0;
+    if(color==1)return 1;
+	if(plc[place][color]!=-1)return plc[place][color];
+
+	long long ans=0;
+	for(int i=1;i<=place;i++){
+		ans += (ncr[place][i]*count(place-i,color-1)%mod);
+		ans %= mod;
+	}
+	return plc[place][color]=ans;
+}
